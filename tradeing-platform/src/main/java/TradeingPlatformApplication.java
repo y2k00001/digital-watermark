809155819776d28d3461d2e0066af1211b15b155
neo <y2k00001@126.com>
2022-12-19 22:31:47 +0800

    feature_lxj_1206: 1.增加碳汇交易模块

---
+++ tradeing-platform/src/main/java/TradeingPlatformApplication.java
@@ -0,0 +1,13 @@
+/**
+ * @author monkey
+ * @date 2022/12/19 22:31
+ */
+
+/**
+ *@ClassName TradeingPlatformApplication
+ * TODO
+ *@Author neo
+ *@date 2022/12/19 22:31
+ */
+public class TradeingPlatformApplication {
+}
