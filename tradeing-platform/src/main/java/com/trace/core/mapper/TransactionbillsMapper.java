aec1bf76488fca2d4587087eb2ee4eb31ce27607
neo <y2k00001@126.com>
2023-05-11 17:34:37 +0800

    trace_v1_dev: 1.优化SQL查询

--- tradeing-platform/src/main/java/com/trace/core/mapper/TransactionbillsMapper.java
+++ tradeing-platform/src/main/java/com/trace/core/mapper/TransactionbillsMapper.java
@@ -15,10 +15,34 @@ import com.trace.core.vo.TransactionbillsListVO;
  */
 public interface TransactionbillsMapper extends BaseMapper<Transactionbills> {
 
+    /**
+     * description
+     * @author monkey
+     * @datetime  2023/5/11 17:34
+     * @param transactionbillsListVO
+     * @return {@link Integer}
+     **/
     Integer confirmContract(TransactionbillsListVO transactionbillsListVO);
+
+    /**
+     * description
+     * @author monkey
+     * @datetime  2023/5/11 17:34
+     * @param transactionbillsListVO
+     * @return {@link Integer}
+     **/
     Integer confirmReceiptCloseContract(TransactionbillsListVO transactionbillsListVO);
 
+    /**
+     * description
+     * @author monkey
+     * @datetime  2023/5/11 17:34
+     * @param page
+     * @param firmId
+     * @return {@link IPage< TransactionbillsListVO>}
+     **/
     IPage<TransactionbillsListVO> showTransactionbillsList(IPage<TransactionbillsListVO> page, Integer firmId);
+
 
     Integer checkStatus(TransactionbillsListVO transactionbillsListVO);
 
